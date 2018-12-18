371ee0d08f9b0019647c980243b82bd722e7c98c
Sam Grill <sgrill@spartaglobal.com>
2018-12-18 14:30:36 +0000

    Update Card.java

--- src/main/java/com/sparta/sdg/Model/Card.java
+++ src/main/java/com/sparta/sdg/Model/Card.java
@@ -1,15 +1,19 @@
 package com.sparta.sdg.Model;
 
 public class Card {
-    private final String suit;
-    private final int value;
+    private final Suits suit;
+    private final CardValues cardValue;
 
-    public Card(int value, String suit){
-        this.value = value;
+    public Card(Suits suit, CardValues cardValue) {
         this.suit = suit;
-
-
-
+        this.cardValue = cardValue;
     }
 
+    public Suits getSuit() {
+        return suit;
+    }
+
+    public CardValues getCardValue() {
+        return cardValue;
+    }
 }
